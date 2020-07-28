62845bf500f494a3516d476774636dc2a8df7ddf
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-07-28 14:59:27 -0400

    refactor: clean up into one php block
    refs:
    issue #6

--- templates/dashboardNav.php
+++ templates/dashboardNav.php
@@ -3,35 +3,61 @@
     <nav class="tabs is-boxed">
       <div class="container">
         <ul>
-          <?php if (strcasecmp($heroTitle, 'jobs') == 0) : ?>
-            <li class="is-active">
-            <?php else : ?>
-            <li>
-            <?php endif; ?>
-            <a href="jobs">Jobs</a>
-            </li>
-            <?php if (strcasecmp($heroTitle, 'job categories') == 0) : ?>
-              <li class="is-active">
-              <?php else : ?>
-              <li>
-              <?php endif; ?>
-              <a href="categories">Job Categories</a>
-              </li>
-              <li><a>Applied Jobs</a></li>
-              <?php if ($_SESSION["isEmployer"]) : ?>
-                <li><a>Posted Jobs</a></li>
-              <?php endif; ?>
-              <?php if ($_SESSION["isAdmin"]) : ?>
-                <li><a>Users</a></li>
-                <li><a>System Activity</a></li>
-              <?php endif; ?>
-              <?php if (strcasecmp($heroTitle, 'my profile') == 0) : ?>
-                <li class="is-active">
-                <?php else : ?>
-                <li>
-                <?php endif; ?>
-                <a href="profile">My Profile</a>
-                </li>
+          <?php
+
+          if (strcasecmp($heroTitle, 'jobs') == 0) {
+            echo '<li class="is-active">';
+          } else {
+            echo '<li>';
+          }
+          echo '<a href="jobs">Jobs</a></li>';
+
+          if (strcasecmp($heroTitle, 'job categories') == 0) {
+            echo '<li class="is-active">';
+          } else {
+            echo '<li>';
+          }
+          echo '<a href="categories">Job Categories</a></li>';
+
+          if (strcasecmp($heroTitle, 'applied jobs') == 0) {
+            echo '<li class="is-active">';
+          } else {
+            echo '<li>';
+          }
+          echo '<a>Applied Jobs</a></li>';
+
+          if ($_SESSION["isEmployer"]) {
+            if (strcasecmp($heroTitle, 'posted jobs') == 0) {
+              echo '<li class="is-active">';
+            } else {
+              echo '<li>';
+            }
+            echo '<a>Posted Jobs</a></li>';
+          }
+
+          if ($_SESSION["isAdmin"]) {
+            if (strcasecmp($heroTitle, 'users') == 0) {
+              echo '<li class="is-active">';
+            } else {
+              echo '<li>';
+            }
+            echo '<a>Users</a></li>';
+
+            if (strcasecmp($heroTitle, 'system activity') == 0) {
+              echo '<li class="is-active">';
+            } else {
+              echo '<li>';
+            }
+            echo '<a>System Activity</a></li>';
+          }
+
+          if (strcasecmp($heroTitle, 'my profile') == 0) {
+            echo '<li class="is-active">';
+          } else {
+            echo '<li>';
+          }
+          echo '<a href="profile">My Profile</a></li>';
+          ?>
         </ul>
       </div>
     </nav>
