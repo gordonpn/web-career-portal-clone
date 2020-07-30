d4bab9283358f50044e55700f7fc35dac103db9d
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-07-30 12:23:10 -0400

    ci: add phpcs config

--- templates/hero.php
+++ templates/hero.php
@@ -1,12 +1,12 @@
 <section class="hero is-info">
-  <?php include "templates/navbar.php"; ?>
+  <?php require "templates/navbar.php"; ?>
   <div class="hero-body">
     <div class="container">
       <h1 class="title">
         <?php echo $heroTitle; ?>
       </h1>
-      <?php include "templates/displayUsername.php"; ?>
+      <?php require "templates/displayUsername.php"; ?>
     </div>
   </div>
-  <?php include "templates/dashboardNav.php"; ?>
+  <?php require "templates/dashboardNav.php"; ?>
 </section>
