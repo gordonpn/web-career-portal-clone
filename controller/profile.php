c39f60d4a6aba63523361dd7744d0eca3659a852
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-01 16:18:00 -0400

    feat: calculate the balance difference after changing plan

--- controller/profile.php
+++ controller/profile.php
@@ -4,14 +4,17 @@ if (!isset($_SESSION)) {
 }
 
 require "model/User.php";
+require "service/balance.php";
 
 class Profile
 {
   public $user;
+  public $balanceService;
 
   public function __construct()
   {
     $this->user = new User();
+    $this->balanceService = new BalanceService();
   }
 
   public function invoke()
@@ -32,11 +35,20 @@ class Profile
 
     if (isset($_POST['newPlan'])) {
       if ($this->user->updatePlan($_SESSION['username'], $_POST['newPlan'])) {
+        $balanceDifference = $this->balanceService->calculateBalanceDifference($_SESSION['planName'], $_POST['newPlan']);
+        $newBalance = $_SESSION['balance'] + $balanceDifference;
+        $_SESSION['balance'] = $newBalance;
         $_SESSION['planName'] = $_POST['newPlan'];
       } else {
         $error = "An error as occurred.";
       }
     }
+
+    if (isset($_SESSION["loggedIn"]) && $_SESSION["balance"] < 0) {
+      include 'view/dashboard.php';
+      return null;
+    }
+
     include 'view/profile.php';
   }
 }
