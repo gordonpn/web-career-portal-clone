d354db32b2f76f7eb74cc05a3d5d875a781b3516
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-04 11:16:16 -0400

    feat: automatically deactivate a user if suffering for 1+ year
    mysql event runs every hour to check each user, if their suffering date
    is more than a year, then set their active status to false
    
    refs:
    issue #41

--- sql/init/3-events.sql
+++ sql/init/3-events.sql
@@ -2,10 +2,35 @@ SET GLOBAL event_scheduler = ON;
 
 DELIMITER $$
 
-CREATE EVENT IF NOT EXISTS automaticCharges
+DROP EVENT IF EXISTS automaticChargesEvent $$
+CREATE EVENT automaticChargesEvent
     ON SCHEDULE EVERY '1' MONTH
         STARTS '2020-09-01 00:00:00'
     DO BEGIN
+END $$
+
+DROP EVENT IF EXISTS deactivateUserEvent $$
+CREATE EVENT deactivateUserEvent
+    ON SCHEDULE EVERY 1 HOUR
+    DO BEGIN
+    DECLARE n int DEFAULT 0;
+    DECLARE i int DEFAULT 1;
+    SELECT count(*) FROM Users INTO n;
+    SET i = 1;
+    WHILE i <= n
+        DO
+            CALL deactivateUser(i);
+            SET i = i + 1;
+        END WHILE;
+END $$
+
+DROP PROCEDURE IF EXISTS deactivateUser $$
+CREATE PROCEDURE deactivateUser(in_userNumber int)
+BEGIN
+    UPDATE Users
+    SET isActive = IF(TIMESTAMPDIFF(DAY, startSufferingDate, CURRENT_TIMESTAMP) >= 365, 0, isActive)
+    WHERE userNumber = in_userNumber;
+
 END $$
 
 DELIMITER ;
