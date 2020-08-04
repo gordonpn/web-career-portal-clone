d141b42bd3aa8311caf9f955a3dde9f4e3c0ad02
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-04 18:36:03 -0400

    feat: modal for adding new payment method
    refs:
    issue #16

--- view/managePaymentMethods.php
+++ view/managePaymentMethods.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION)) {
         <?php if (isset($paymentMethods) && count($paymentMethods) > 1) : ?>
           <button class="button is-link is-outlined is-right" id="change-pre-selected-btn">Change Pre-Selected</button>
         <?php endif; ?>
-        <button class="button is-link is-outlined is-right">Add Payment Method</button>
+        <button class="button is-link is-outlined is-right" id="add-payment-btn">Add Payment Method</button>
       </div>
       <table class="table is-bordered is-striped is-narrow is-hoverable is-fullwidth">
         <thead>
@@ -61,7 +61,7 @@ if (!isset($_SESSION)) {
               echo 'Edit';
               echo '</a>';
               echo '</td>';
-              if (count($paymentMethods) > 1 && !$obj->isPreSelected) {
+              if (!$obj->isPreSelected && count($paymentMethods) > 1) {
                 echo '<td>';
                 echo "<a class=\"button is-small is-danger is-light\" href=\"managePaymentMethods?deletePaymentMethod=$obj->paymentMethodID\">";
                 echo 'Delete';
@@ -81,42 +81,81 @@ if (!isset($_SESSION)) {
     <div class="modal-card">
       <header class="modal-card-head">
         <p class="modal-card-title">Change Pre-Selected Payment Method</p>
-        <a class="delete" aria-label="close" id="delete-change-pre-selected-close-btn"></a>
+        <a class="delete" aria-label="close" id="change-pre-selected-close-btn"></a>
       </header>
       <section class="modal-card-body">
-        <form action="managePaymentMethods" method="POST"">
+        <form action="managePaymentMethods" method="POST">
           <div class=" field has-addons has-addons-centered">
-          <p class="control">
-            <span class="select">
-              <select name="updatePreSelected" required>
-                <?php
-                if (isset($paymentMethods)) {
-                  foreach ($paymentMethods as $index => $obj) {
-                    echo "<option value=\"$obj->paymentMethodID\">$obj->paymentType: $obj->cardNumber</option>";
+            <p class="control">
+              <span class="select">
+                <select name="updatePreSelected" required>
+                  <?php
+                  if (isset($paymentMethods)) {
+                    foreach ($paymentMethods as $index => $obj) {
+                      echo "<option value=\"$obj->paymentMethodID\">$obj->paymentType: $obj->cardNumber</option>";
+                    }
                   }
-                }
-                ?>
-              </select>
-            </span>
-          </p>
-          <p class="control">
-            <button class="button is-info" type="submit" id="select-pre-selected-btn">
-              Select as Default
-            </button>
-          </p>
+                  ?>
+                </select>
+              </span>
+            </p>
+            <p class="control">
+              <button class="button is-info" type="submit" id="select-pre-selected-btn">
+                Select as Default
+              </button>
+            </p>
+          </div>
+        </form>
+      </section>
     </div>
-    </form>
-    </section>
   </div>
+  <div class="modal" id="modal-add">
+    <div class="modal-background"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Add Payment Method</p>
+        <a class="delete" aria-label="close" id="add-method-close-btn"></a>
+      </header>
+      <section class="modal-card-body">
+        <form action="managePaymentMethods" method="POST">
+          <div class=" field has-addons has-addons-centered">
+            <p class="control">
+              <div class="select">
+                <select name="newPaymentType" required>
+                  <option value="Credit">Credit</option>
+                  <option value="Debit">Debit</option>
+                </select>
+              </div>
+            </p>
+            <p class="control has-icons-left">
+              <input name="newCardNumber" class="input" type="tel" inputmode="numeric" pattern="[0-9]{4,19}" autocomplete="cc-number" maxlength="19" placeholder="xxxx xxxx xxxx xxxx" required>
+              <span class="icon is-small is-left">
+                <i class="far fa-credit-card"></i>
+              </span>
+            </p>
+            <p class="control">
+              <button class="button is-info" type="submit">
+                Add Payment Method
+              </button>
+            </p>
+          </div>
+        </form>
+      </section>
+    </div>
   </div>
   <script type="text/javascript">
     const changePreSelectedModal = document.getElementById("modal-change-preselected");
-    const changePreSelectedCloseButton = document.getElementById("delete-change-pre-selected-close-btn");
+    const changePreSelectedCloseButton = document.getElementById("change-pre-selected-close-btn");
     const changePreSelectedButton = document.getElementById("change-pre-selected-btn");
+    const addPaymentButton = document.getElementById("add-payment-btn");
+    const addPaymentCloseButton = document.getElementById("add-method-close-btn");
+    const addPaymentModal = document.getElementById("modal-add");
 
     function closeModal() {
       changePreSelectedModal.className = "modal";
+      addPaymentModal.className = "modal";
     }
+
     if (changePreSelectedButton) {
       changePreSelectedButton.addEventListener("click", function() {
         changePreSelectedModal.className = "modal is-active";
@@ -124,6 +163,14 @@ if (!isset($_SESSION)) {
     }
     if (changePreSelectedCloseButton) {
       changePreSelectedCloseButton.addEventListener('click', closeModal);
+    }
+    if (addPaymentButton) {
+      addPaymentButton.addEventListener("click", function() {
+        addPaymentModal.className = "modal is-active";
+      });
+    }
+    if (addPaymentCloseButton) {
+      addPaymentCloseButton.addEventListener('click', closeModal);
     }
   </script>
   <script defer src="https://use.fontawesome.com/releases/v5.3.1/js/all.js"></script>
