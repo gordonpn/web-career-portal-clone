8b6810f947e68744cb35c904cf80789c96fdcbb2
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-05 06:52:15 -0400

    fix: redirection after login in
    make action of form point to dashboard, because that is where we want
    the user to be after they successfully log in.

--- controller/dashboard.php
+++ controller/dashboard.php
@@ -34,6 +34,16 @@ class Dashboard
       }
     }
 
+    if (isset($_POST["username"]) && isset($_POST["password"])) {
+      $username = filter_var(trim($_POST["username"]), FILTER_SANITIZE_STRING);
+      $password = $_POST["password"];
+      $error = $this->checkUser($username, $password);
+      if (!is_null($error)) {
+        include 'view/login.php';
+        return null;
+      }
+    }
+
     if (isset($_SESSION["loggedIn"])) {
       $paymentMethods = $this->paymentMethod->getPaymentMethodsOf($_SESSION['username']);
     }
@@ -45,5 +55,49 @@ class Dashboard
   {
     session_unset();
     session_destroy();
+  }
+
+  public function checkUser($username, $password)
+  {
+    $user = $this->user->getUser($username, $password);
+
+    if (is_null($user)) {
+      return "User does not exist";
+    }
+
+    $userVars = get_object_vars($user);
+    foreach ($userVars as $key => &$value) {
+
+      if ($key == 'userType') {
+        switch ($value):
+          case 'admin':
+            $_SESSION["isAdmin"] = true;
+            $_SESSION["isEmployer"] = false;
+            $_SESSION["isEmployee"] = false;
+            break;
+          case 'employer':
+            $_SESSION["isAdmin"] = false;
+            $_SESSION["isEmployer"] = true;
+            $_SESSION["isEmployee"] = false;
+            break;
+          case 'employee':
+            $_SESSION["isAdmin"] = false;
+            $_SESSION["isEmployer"] = false;
+            $_SESSION["isEmployee"] = true;
+            break;
+        endswitch;
+      }
+      $_SESSION[$key] = $value;
+    }
+    unset($value);
+
+    if (!$_SESSION['isActive']) {
+      session_unset();
+      session_destroy();
+      return "Your account is deactivated. Contact an administrator to reactivate your account.";
+    }
+
+    $_SESSION["loggedIn"] = true;
+    return null;
   }
 }
--- controller/login.php
+++ controller/login.php
@@ -17,60 +17,6 @@ class Login
 
   public function invoke()
   {
-    if (isset($_POST["username"]) && isset($_POST["password"])) {
-      $username = filter_var(trim($_POST["username"]), FILTER_SANITIZE_STRING);
-      $password = $_POST["password"];
-      $this->checkUser($username, $password);
-      return null;
-    }
     include 'view/login.php';
-  }
-
-  public function checkUser($username, $password)
-  {
-    $user = $this->user->getUser($username, $password);
-
-    if (is_null($user)) {
-      $error = "User does not exist";
-      include 'view/login.php';
-      return null;
-    }
-
-    $userVars = get_object_vars($user);
-    foreach ($userVars as $key => &$value) {
-
-      if ($key == 'userType') {
-        switch ($value):
-          case 'admin':
-            $_SESSION["isAdmin"] = true;
-            $_SESSION["isEmployer"] = false;
-            $_SESSION["isEmployee"] = false;
-            break;
-          case 'employer':
-            $_SESSION["isAdmin"] = false;
-            $_SESSION["isEmployer"] = true;
-            $_SESSION["isEmployee"] = false;
-            break;
-          case 'employee':
-            $_SESSION["isAdmin"] = false;
-            $_SESSION["isEmployer"] = false;
-            $_SESSION["isEmployee"] = true;
-            break;
-        endswitch;
-      }
-      $_SESSION[$key] = $value;
-    }
-    unset($value);
-
-    if (!$_SESSION['isActive']) {
-      session_unset();
-      session_destroy();
-      $error = "Your account is deactivated. Contact an administrator to reactivate your account.";
-      include 'view/login.php';
-      return null;
-    }
-
-    $_SESSION["loggedIn"] = true;
-    include 'view/dashboard.php';
   }
 }
