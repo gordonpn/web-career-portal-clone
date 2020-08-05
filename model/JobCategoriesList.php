2e53f302b7f1221c2f798bd970159750cc78df86
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-05 17:41:54 -0400

    fix: bug where inserting two same category name will kaput
    refs:
    issue #49

--- model/JobCategoriesList.php
+++ model/JobCategoriesList.php
@@ -11,13 +11,21 @@ class JobCategoriesList
     $this->_db = new Database();
   }
 
+  public function findJobCategory($categoryName)
+  {
+    $sql = "SELECT jobCategoriesID FROM Job_Categories_List WHERE categoryName = :categoryName";
+    $this->_db->query($sql);
+    $this->_db->bind(':categoryName', $categoryName, PDO::PARAM_STR);
+    return $this->_db->fetchOne();
+  }
+
   public function createJobCategory($categoryName)
   {
     $sql = "INSERT INTO Job_Categories_List(categoryName)
-    VALUES (:categoryName)
-    ON DUPLICATE KEY UPDATE categoryName=categoryName";
+    VALUES (:categoryName)";
     $this->_db->query($sql);
     $this->_db->bind(':categoryName', $categoryName, PDO::PARAM_STR);
-    return $this->_db->lastInsertId();
+    $lastID = $this->_db->lastInsertId();
+    return $lastID;
   }
 }
