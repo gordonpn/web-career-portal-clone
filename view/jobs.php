43299b90bcc2b8e162136c58ec40687ced5a4e57
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-05 07:53:34 -0400

    feat: add modal dynamically for each job posting
    refs:
    issue #35

--- view/jobs.php
+++ view/jobs.php
@@ -49,7 +49,6 @@ if (!isset($_SESSION)) {
             <th>Date Posted</th>
             <th>Description</th>
             <th>Company Name</th>
-            <th>Employer Name</th>
             <th>Location</th>
             <th>Salary</th>
             <th>Positions Available</th>
@@ -60,15 +59,18 @@ if (!isset($_SESSION)) {
         </thead>
         <tbody>
           <?php
-          foreach ($jobs as $key => $value) {
+          foreach ($jobs as $index => $obj) {
             echo '<tr>';
-            foreach ($value as $propName => $propValue) {
-              echo "<td>$propValue</td>";
-            }
+            echo "<td>$obj->title</td>";
+            echo "<td>$obj->datePosted</td>";
+            echo "<td>$obj->description</td>";
+            echo "<td>$obj->companyName</td>";
+            echo "<td>$obj->city</td>";
+            echo "<td>$obj->salary</td>";
+            echo "<td>$obj->positionsAvailable</td>";
+            echo "<td>$obj->status</td>";
             echo '<td>';
-            echo "<a class=\"button is-small is-info\">";
-            echo 'Contact';
-            echo '</a>';
+            include "templates/contactModal.php";
             echo '</td>';
             echo '<td>';
             echo "<a class=\"button is-small is-success\">";
