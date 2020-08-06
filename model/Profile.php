f5c5697909c584608f455f2f6ee827a3719dafe1
Gordon Pham-Nguyen <gordon.pn6@gmail.com>
2020-08-06 11:49:46 -0400

    feat: employer can update their category name

--- model/Profile.php
+++ model/Profile.php
@@ -19,17 +19,19 @@ class ProfileModel
     return $this->_db->execute();
   }
 
-  public function updateProfile($username, $firstName, $lastName, $phoneNumber, $companyName)
+  public function updateProfile($username, $firstName, $lastName, $phoneNumber, $companyName, $categoryName)
   {
-    $sql = "UPDATE Profiles
-    SET firstName = :firstName, lastName = :lastName, phoneNumber = :phoneNumber, companyName = :companyName
-    WHERE userID = :username";
+    $sql = "UPDATE Profiles, Employer_Categories
+    SET firstName = :firstName, lastName = :lastName, phoneNumber = :phoneNumber, companyName = :companyName, categoryName = :categoryName
+    WHERE Profiles.userID = :username AND Employer_Categories.userID = :userID";
     $this->_db->query($sql);
     $this->_db->bind(':username', $username, PDO::PARAM_STR);
+    $this->_db->bind(':userID', $username, PDO::PARAM_STR);
     $this->_db->bind(':firstName', $firstName, PDO::PARAM_STR);
     $this->_db->bind(':lastName', $lastName, PDO::PARAM_STR);
     $this->_db->bind(':companyName', $companyName, PDO::PARAM_STR);
     $this->_db->bind(':phoneNumber', $phoneNumber, PDO::PARAM_STR);
+    $this->_db->bind(':categoryName', $categoryName, PDO::PARAM_STR);
     return $this->_db->execute();
   }
 
